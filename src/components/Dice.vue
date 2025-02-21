be282729afad2030485740239051972a9619823b
Noctyris <noctyris@proton.me>
2025-02-21 17:33:49 +0100

    Update

---
+++ src/components/Dice.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    props: [
+      'num',
+      'isLatest'
+    ]
+  }
+</script>
+
+<template>
+  <img :src="`/images/dice${num}.svg`" :id="isLatest ? 'latest' : 'old'" :alt="`Résultat du dé: ${num}`">
+</template>
+
+<style scoped>
+img {
+  width: 15%;
+  border: 2px solid var(--color-border);
+  border-radius: 10%;
+  transition: .5s;
+}
+img:hover {
+  border: 2px solid var(--color-border-hover);
+}
+#latest {
+  width: 25%;
+}
+</style>
---
+++ src/components/RollButton.vue
@@ -0,0 +1,4 @@
+<template>
+  <button>Lancer</button>
+</template>
+
